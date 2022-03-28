2d3afb3d0942e33e428ca68a2c4ec5dacf17432c
AidilAhda <76725012+AidilAhda@users.noreply.github.com>
2022-03-28 23:10:59 +0700

    button to about

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,40 +2,53 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <div class="hello">
-    <div class="name">
-      <div class="letter">
-        <h4> <span>Hey</span>  this is my homepage, so I have to say something about myself. Sometimes it is hard to introduce yourself because you know yourself so well that you do not know where to start with. Let me give a try to see what kind of image you have about me through my self-description. I hope that my impression about myself and your impression about me are not so different. Here it goes</h4>
-
-      </div>
-    </div>
-    <a href="../views/AboutView.vue"> Download My CV</a>
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1"> Hello,My Name is </div>
+                <div class="text-2"> Aidil Ahda</div>
+                <div class="text-3"> and I'm a <span>Student</span></div>
+                <router-link to="/about">my cv</router-link> 
+            </div>
+        </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.letter{
-  margin-top: 18%;
+/* home styling */ 
+
+
+.home .max-width {
+  margin: auto 0 auto 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.home .home-content .text-1 {
+  font-size: 27px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.home .home-content .text-2 {
+  font-size: 75px;
+  font-weight: 600;
+  margin-left: -3px;
 }
-a {
-  color: #42b983;
+
+.home .home-content .text-3 {
+  font-size: 40px;
+  margin: 5px 0;
+}
+
+.home .home-content .text-3 span {
+  color: rgb(252, 48, 252);
+  font-weight: 500;
+}
+
+.home .home-content a {
   display: inline-block;
   background-color: rgb(211, 19, 211);
   color: #fff;
@@ -46,12 +59,9 @@ a {
   border: 2px solid rgb(211, 19, 211);
   transition: all 0.3s ease;
 }
-a:hover{
+
+.home .home-content a:hover {
   color: rgb(252, 48, 252);
   background: none;
-}
-span{
- color: rgb(252, 48, 252);
-  font-size: 34px;
 }
 </style>
